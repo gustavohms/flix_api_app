d9f6e5af1eaa042d20bf4634b3b51608cde2c9dd
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-04-10 06:58:08 -0300

    feat: Enhance services to utilize Streamlit session state for caching and add configuration files

--- actors/service.py
+++ actors/service.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from actors.repository import ActorRepository
 
 class ActorService:
@@ -5,7 +6,11 @@ class ActorService:
         self.actor_repository = ActorRepository()
     
     def get_actors(self):
-        return self.actor_repository.get_actors()
+        if 'actors' in st.session_state:
+            return st.session_state.actors
+        actors = self.actor_repository.get_actors()
+        st.session_state = actors
+        return actors
     
     def create_actor(self, name, birthday, nationality):
         actor = dict(
@@ -13,4 +18,6 @@ class ActorService:
             birthday=birthday,
             nationality = nationality
         )
-        return self.actor_repository.create_actor(actor)
+        new_actor = self.actor_repository.create_actor(actor)
+        st.session_state.actors.append(new_actor)
+        return new_actor
